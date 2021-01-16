7cc6ebe34fcb616cb58b4873594f3d9af514f817
Fadimata Tillman <fadi.tillman@gmail.com>
2021-01-15 20:13:24 -0800

    creation checkout Total component

--- src/CheckoutTotal.js
+++ src/CheckoutTotal.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useStateValue} from "./StateProvider";
-import "./CheckoutItem.css"
 
-function CheckoutItem({id, title, image, price, rating}) {
+
+function CheckoutTotal({id, title, price}) {
     const [{basket}, dispatch] = useStateValue();
     return (
         <div className="checkoutitems">
@@ -10,26 +10,17 @@ function CheckoutItem({id, title, image, price, rating}) {
            <div className="checkoutitems-info">
            
                <p className="checkoutitme_title">{title}</p>
-               <img  className="checkoutitems-image" src={image}  alt=""/>
-
-
+              
                 <p className="checkoutitems-price" > 
                 <small>$</small>
                 <strong>{price}</strong>
                 </p>
-                <div className="checkoutitems-rating">
-                {Array(rating).fill().map ((_, i) => (<p>⭐️</p> ))}
-                </div>
-           
-            <div>  
-            
-            <button >Delete</button> 
-           </div>
-                 
+               
+          
                  </div>
                  </div>
       
     )
 }
 
-export default CheckoutItem
+export default CheckoutTotal
--- src/Total.js
+++ src/Total.js
@@ -3,24 +3,38 @@ import   "./Total.css";
 import CurrencyFormat from "react-currency-format";
 import{BasketItems} from "./Reducer";
 import {useStateValue} from "./StateProvider";
+import CheckoutTotal from './CheckoutTotal';
+
+
 
 function Total() {
     const [{basket}, dispatch]= useStateValue();
     
     return (
-    
+       <div>
         <div className="subtotal">
            <CurrencyFormat
           renderText={(value)=> (
               <> 
               <p>
-                  Total({basket.length} Items):
-          <strong> {value}</strong>
+                  detail({basket.length} items):
+         
+          {basket.map (item => (
+            <CheckoutTotal
+            id={item.id} 
+            title= {item.title}
+            price= {item.price}
+            
+            />
+               ))}
+               <hr></hr>
+                <strong> Total:{value}</strong>
               </p>
               {/* <small className="subtotal-gift">
                   <input type="checkbox"/>This order
                   contains a gift
               </small> */}
+             
                   </>
           )}
           decimalScale={2}
@@ -30,6 +44,7 @@ function Total() {
           prefix={"$"}
           /> 
           <button> Proceed to checkout</button>
+        </div>
         </div>
     )
 }
