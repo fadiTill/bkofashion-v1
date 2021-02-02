83cfd25f284ef201b614c21700d53b4502926b39
Fadimata Tillman <fadi.tillman@gmail.com>
2021-02-01 19:59:58 -0800

    styling

--- src/Header.js
+++ src/Header.js
@@ -17,7 +17,7 @@ function Header() {
     return (
         <div className="header">
           <Link to="/" >
-          <img className="header_logo"src="images/bkologo.jpg" alt="logo"/> 
+          <img className="header_logo"src="images/logo-bkofashion.jpg" alt="logo"/> 
           </Link>
         <div className="header_search">
           <input className="header_searchInput" type="text"/>
