b610fadeafadb4330a40a424b90d68f720c71bc9
Fadimata Tillman <fadi.tillman@gmail.com>
2021-02-11 23:44:23 -0800

    styling

--- src/Carrousel.js
+++ src/Carrousel.js
@@ -5,7 +5,9 @@ export default function Carrousel() {
         
     
         <div class="slideshow-container">
-
+<img className="header_logo"src="images/logo-bkofashion.jpg" alt="logo"/> 
+<img className="header_logo"src="images/logo-bkofashion.jpg" alt="logo"/> 
+<img className="header_logo"src="images/logo-bkofashion.jpg" alt="logo"/> 
             </div>
             
     
