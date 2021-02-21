afbbbe2a4fd0c114348654df94b6cea505ce6956
Fadimata Tillman <fadi.tillman@gmail.com>
2021-02-20 22:00:16 -0800

    carousel

--- src/ImgCarousel.js
+++ src/ImgCarousel.js
@@ -5,22 +5,44 @@ import "./ImgCarousel.css"
 
 
 const ImgCarousel  = ({carousels})  => {
- const [current, setCurrent] = useState(0)
-  const length = carousels.length
+const [current, setCurrent] = useState(0)
+const length = carousels.length
+
+  const nextCarousel = () => {
+  setCurrent(current === length - 1 ? 0 :  current + 1 );
+
+  }
+
+  const prevCarousel = () => {
+    setCurrent(current ===  0 ? length - 1 :  current - 1 );
+  
+    }
+
+     console.log(current)
  
- 
+  if(!Array.isArray(carousels) || carousels.length <= 0) {
+ return null;
+}
+
+
 
     return (
         <section className="slider">
-             {/* < FaarrowAltCircleLeft  className="left Arrow " onclick={prevscarousel}/> */}
-             {/* < FaarrowAltCircRight  className="Right Arrow " onclick={nextcarousel}/> */}
-            < FaArrowAltCircleLeft  className="Left-arrow " />
-            < FaArrowAltCircleRight  className="Right-arrow "/>
+           
+            < FaArrowAltCircleLeft  className="Left-arrow " onclick={prevCarousel} />
+            < FaArrowAltCircleRight  className="Right-arrow " onclick={nextCarousel}/>
             {CarouselData.map((carousel, index) => {
+                return (
+                <div className={index === current ? 'carousel active' : 'carousel' } key={index} >
+                {index === current &&(<img  className="home-image" src={carousel.image}  alt="carousel image"/>
+                    )}
+                
+                </div>
 
-                return <img  className="home-image" src={carousel.image}  alt="carousel image"/>;
+                )
                 
 
+    
             })}
         </section>
     );
